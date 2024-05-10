a19113ec7cf15e448945896b3464f3babcb5a5f5
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-10 23:21:52 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
+    int count = 0;
 
+    for (int num = 100; num <= 999; ++num) {
+        int cotni = num / 100;
+        int decyti = (num / 10) % 10;
+        int odinutsi = num % 10;
+
+        if (cotni != decyti && cotni != odinutsi && decyti != odinutsi) {
+            ++count;
+        }
+    }
+
+    cout << "Result: " << count << endl;
     system("pause");
     return 0;
 }
