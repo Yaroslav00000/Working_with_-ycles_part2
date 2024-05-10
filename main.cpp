bf0641e894526c5f87375385d6feb42982e5f239
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-10 23:29:49 +0300

    task complited

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
+    int number;
+    cout << "Enter number: ";
+    cin >> number;
+
+    int rezult = 0;
+    int pozition = 1;
+
+    for (; number != 0; number /= 10) {
+        int digit = number % 10;
+        if (digit != 3 && digit != 6) {
+            rezult += digit * pozition;
+            pozition *= 10;
+        }
+    }
+
+    cout << "Rezult: " << rezult << endl;
 
     system("pause");
     return 0;
