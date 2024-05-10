ba34400a6cabb3b89c23eb10f4f2aa83f69922fe
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-10 22:24:13 +0300

    Task complite

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
+    int second, hours, minutes, seconds;
 
+    cout << "Enter seconds: ";
+    cin >> second;
+
+    hours = second / 3600;
+    minutes = (second % 3600) / 60;
+    seconds = second % 60;
+
+    cout << "Result: " << hours << " hours " << minutes << " minutes " << seconds << " seconds" << endl;
     system("pause");
     return 0;
 }
