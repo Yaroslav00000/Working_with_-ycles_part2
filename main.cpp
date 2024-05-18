a5f5bdf226d41b9bfe6876c0ba43674dca8842fb
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-18 18:57:48 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    int a;
+    cout << "¬вед≥ть ц≥ле число A: ";
+    cin >> a;
 
-    system("pause");
+    for (int b = 1; b <= a; b++) {
+        if (a % (b * b) == 0 && a % (b * b * b) != 0) {
+            cout << b << " ";
+        }
+    }
+
+    cout << endl;
     return 0;
 }
