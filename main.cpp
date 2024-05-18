196a2131cc2429e315324d4330d51137f03259ee
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-18 21:11:07 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    int num1, num2;
 
-    system("pause");
+    cout << "¬вед≥ть перше ц≥ле число: ";
+    cin >> num1;
+
+    cout << "¬вед≥ть друге ц≥ле число: ";
+    cin >> num2;
+
+    cout << "—п≥льн≥ д≥льники: ";
+
+
+    int min = min(num1, num2);
+    for (int dil = 1; dil <= min; dil++) {
+        if (num1 % dil == 0 && num2 % dil == 0) {
+            cout << dil << " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
