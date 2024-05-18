fd8a1ebfefbf60561666ed2165350c5f75ba222c
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-18 19:52:25 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include <Windows.h>
+#include <windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    int a; 
 
-    system("pause");
+    cout << "Введіть ціле число: ";
+    cin >> a;
+
+    int sum = 0;
+    int temp = a;
+	for (int i = 0; i < 3; i++) {
+		sum += temp % 10;
+		temp /= 10;
+	}
+
+    int kubsum = sum * sum * sum;
+    if (kubsum == a * a) {
+        cout << a << " - число, для якого куб суми цифр дорівнює A * A." << endl;
+    }
+    else {
+        cout << a << " - число, для якого куб суми цифр не дорівнює A * A." << endl;
+    }
+
     return 0;
 }
