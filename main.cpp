25c21951fb998f3821089c7e90a8533932a2155d
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-18 19:58:06 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <Windows.h>
+
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    int number;
 
-    system("pause");
+    cout << "Введіть ціле число: ";
+    cin >> number;
+
+    cout << "Дільники числа " << number << ": ";
+    for (int dilnuk = 1; dilnuk <= number; dilnuk++) {
+        if (number % dilnuk == 0) {
+            int pardilnik = number / dilnuk;
+            if (pardilnik != dilnuk) {
+                cout << pardilnik << " ";
+            }
+        }
+    }
+    cout << endl;
     return 0;
 }
